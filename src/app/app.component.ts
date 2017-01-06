267acff41bde8486cf93755dada8a93c8793ead3
Juri <juri.strumpflohner@gmail.com>
2017-01-06 15:42:16 +0100

    add user icons etc.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AngularFire, FirebaseListObservable, AuthProviders, AuthMethods, FirebaseAuthState } from 'angularfire2';
 import * as moment from 'moment';
+import { DomSanitizer  } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +9,27 @@ import * as moment from 'moment';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  isEditMode: boolean = false;
   items: FirebaseListObservable<any[]>;
+  users: FirebaseListObservable<any[]>;
+  retrievedUsers: any[];
   userStandup: FirebaseListObservable<any[]>;
   auth: FirebaseAuthState;
   today = moment().format('YYYY-MM-DD');
 
-  constructor(public af: AngularFire) {
+  constructor(public af: AngularFire, public santizier: DomSanitizer) {
     this.af.auth.subscribe(auth => {
       this.auth = auth;
       console.log('Auth', auth);
 
-      this.af.database.list('/users')
+      this.users = this.af.database.list('/users');
+      this.users.subscribe((data) => {
+        this.retrievedUsers = data;
+        console.log('users', this.retrievedUsers);
+      });
+
+      // make sure the current user is being added to the list of all users
+      this.users
         .update(this.auth.uid, {
           name: this.auth.auth.displayName,
           email: this.auth.auth.email,
@@ -33,6 +44,11 @@ export class AppComponent {
     this.af.auth.login();
   }
 
+  getProfilePicUrl(uid) {
+    const user = this.retrievedUsers.find(x => x.$key === uid);
+    return this.santizier.bypassSecurityTrustStyle(`url(${user.photoUrl}`);
+  }
+
   logout() {
     console.log('calling logout');
     this.af.auth.logout();
@@ -41,8 +57,6 @@ export class AppComponent {
   fetchData() {
     this.items = this.af.database.list(`/testproject/standups/${this.today}`);
 
-    console.debug('fetching data with for ', this.today);
-
     // logging
     this.af.database.list('/testproject/standups')
       .subscribe((data) => {
@@ -50,15 +64,31 @@ export class AppComponent {
       });
   }
 
+  addStandupNotes() {
+    this.isEditMode = true;
+  }
+
   getList(key) {
     return this.af.database.list(`/testproject/standups/${key}`);
   }
 
   saveStandup(newStandup) {
-    const newObj = {};
-    newObj[this.auth.uid] = newStandup;
+    newStandup.user = {
+      uid: this.auth.uid,
+      name: this.auth.auth.displayName
+    };
+    this.items.update(this.auth.uid, newStandup);
 
-    this.items.update(this.today, newObj);
+    this.isEditMode = false;
+  }
+
+  deleteEntry(entry) {
+    const itemObservable = this.af.database.object(`/testproject/standups/${this.today}/${entry.$key}`);
+    itemObservable.remove();
+  }
+
+  cancel() {
+    this.isEditMode = false;
   }
 
 }
